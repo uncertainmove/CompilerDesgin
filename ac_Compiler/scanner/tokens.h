347d1803968c968d16c1288d62f974387960141e
uncertainmove <hust.zhenzeng@gmail.com>
2016-09-15 20:58:26 +0800

    add(scanner_produce.l): Makefile need to fix

---
+++ ac_Compiler/scanner/tokens.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: tokens.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年09月15日 星期四 20时07分31秒
+ ************************************************************************/
+
+#ifndef _TOKENS_H
+#define _TOKENS_H
+
+#include <stdio.h>
+
+#define FLOATDCL 0
+#define INTDCL 1
+#define PRINT 2
+#define ID 3
+#define NUM 4
+#define ASSIGN 5
+#define PLUS 6
+#define MINUS 7
+#endif
